0cf7ff4ac9135cd9798cd0a5e4aa2deaaa69faf7
Volodya <minenkovvladimir872@gmail.com>
2024-11-17 11:25:02 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,18 +1,16 @@
 import sys
-from PyQt6.QtCore import Qt, QDate, QSize
+from PyQt6.QtCore import Qt, QDate
 from PyQt6.QtWidgets import (
     QApplication,
     QMainWindow,
     QWidget,
     QVBoxLayout,
     QHBoxLayout,
-    QListWidget,
     QListWidgetItem,
     QPushButton,
     QLineEdit,
     QLabel,
     QMessageBox,
-    QMenu,
     QDateEdit,
     QComboBox,
     QFormLayout,
@@ -20,53 +18,8 @@ from PyQt6.QtWidgets import (
     QSizePolicy,
 )
 from PyQt6.QtGui import QColor
+from task_tracker import TaskTracker
 
-
-class TaskTracker(QListWidget):
-    def __init__(self):
-        super().__init__()
-        self.setDragEnabled(True)
-        self.setAcceptDrops(True)
-        self.setDropIndicatorShown(True)
-        self.setSelectionMode(QListWidget.SelectionMode.MultiSelection)
-
-    def dropEvent(self, event):
-        if event.source() == self:
-            super().dropEvent(event)
-        else:
-            event.acceptProposedAction()
-            for item in event.source().selectedItems():
-                self.addItem(item.clone())
-                event.source().takeItem(event.source().row(item))
-
-    def clone(self):
-        return QListWidgetItem(self.text())
-
-    def contextMenuEvent(self, event):
-        context_menu = QMenu(self)
-        delete_action = context_menu.addAction("Удалить задачу")
-        action = context_menu.exec(event.globalPos())
-
-        if action == delete_action:
-            self.delete_selected_tasks()
-
-    def delete_selected_tasks(self):
-        selected_tasks = self.selectedItems()
-        if selected_tasks:
-            reply = QMessageBox.question(
-                self,
-                "Подтверждение удаления",
-                "Вы уверены, что хотите удалить выбранные задачи?",
-                QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No,
-                QMessageBox.StandardButton.No,
-            )
-            if reply == QMessageBox.StandardButton.Yes:
-                for task in selected_tasks:
-                    self.takeItem(self.row(task))
-        else:
-            QMessageBox.warning(
-                self, "Ошибка", "Пожалуйста, выберите задачу для удаления."
-            )
 
 
 class Manager(QMainWindow):
