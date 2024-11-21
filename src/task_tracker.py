8f45ba9b11a351d8c964ff4d7c43e5764f25c1a6
Volodya <minenkovvladimir872@gmail.com>
2024-11-21 20:11:36 +0300

    Update task_tracker.py

--- src/task_tracker.py
+++ src/task_tracker.py
@@ -1,30 +1,41 @@
-from PyQt6.QtWidgets import QListWidget, QListWidgetItem, QMessageBox, QMenu
+from task_tracker import *
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QMessageBox, QListWidget, QMenu
 
 
 class TaskTracker(QListWidget):
-    def __init__(self):
+    def __init__(self, category, db_connection):
         super().__init__()
+        self.category = category
+        self.db_connection = db_connection
         self.setDragEnabled(True)
         self.setAcceptDrops(True)
         self.setDropIndicatorShown(True)
         self.setSelectionMode(QListWidget.SelectionMode.MultiSelection)
+        self.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.show_context_menu)
 
     def dropEvent(self, event):
-        if event.source() == self:
-            super().dropEvent(event)
-        else:
+        if event.source() != self:
             event.acceptProposedAction()
             for item in event.source().selectedItems():
-                self.addItem(item.clone())
+                task_id = item.data(Qt.ItemDataRole.UserRole)
+                self.add_task_to_category(task_id, item)
                 event.source().takeItem(event.source().row(item))
+                self.addItem(item)
 
-    def clone(self):
-        return QListWidgetItem(self.text())
+    def add_task_to_category(self, task_id, item):
+        cursor = self.db_connection.cursor()
+        cursor.execute(
+            "UPDATE tasks SET category = ? WHERE id = ?", (self.category, task_id)
+        )
+        self.db_connection.commit()
+        self.addItem(item)
 
-    def contextMenuEvent(self, event):
+    def show_context_menu(self, position):
         context_menu = QMenu(self)
         delete_action = context_menu.addAction("Удалить задачу")
-        action = context_menu.exec(event.globalPos())
+        action = context_menu.exec(self.mapToGlobal(position))
 
         if action == delete_action:
             self.delete_selected_tasks()
@@ -41,8 +52,15 @@ class TaskTracker(QListWidget):
             )
             if reply == QMessageBox.StandardButton.Yes:
                 for task in selected_tasks:
+                    self.remove_task_from_db(task)
                     self.takeItem(self.row(task))
         else:
             QMessageBox.warning(
                 self, "Ошибка", "Пожалуйста, выберите задачу для удаления."
             )
+
+    def remove_task_from_db(self, item):
+        task_id = item.data(Qt.ItemDataRole.UserRole)
+        cursor = self.db_connection.cursor()
+        cursor.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
+        self.db_connection.commit()
