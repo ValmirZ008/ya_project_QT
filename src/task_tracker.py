71a4f01b6518feb21be087d28ec9f3492a1922ea
Volodya <minenkovvladimir872@gmail.com>
2024-11-24 17:00:08 +0300

    Update task_tracker.py

--- src/task_tracker.py
+++ src/task_tracker.py
@@ -4,35 +4,27 @@ from PyQt6.QtWidgets import QMessageBox, QListWidget, QMenu
 
 
 class TaskTracker(QListWidget):
-    def __init__(self, category, db_connection):
+    def __init__(self, category, db_manager):
         super().__init__()
         self.category = category
-        self.db_connection = db_connection
+        self.db_manager = db_manager
         self.setDragEnabled(True)
         self.setAcceptDrops(True)
         self.setDropIndicatorShown(True)
         self.setSelectionMode(QListWidget.SelectionMode.MultiSelection)
         self.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
-        self.customContextMenuRequested.connect(self.show_context_menu)
+        self.customContextMenuRequested.connect(self.delete_menu)
 
     def dropEvent(self, event):
         if event.source() != self:
             event.acceptProposedAction()
             for item in event.source().selectedItems():
                 task_id = item.data(Qt.ItemDataRole.UserRole)
-                self.add_task_to_category(task_id, item)
+                self.db_manager.update_task_category(task_id, self.category)
                 event.source().takeItem(event.source().row(item))
                 self.addItem(item)
 
-    def add_task_to_category(self, task_id, item):
-        cursor = self.db_connection.cursor()
-        cursor.execute(
-            "UPDATE tasks SET category = ? WHERE id = ?", (self.category, task_id)
-        )
-        self.db_connection.commit()
-        self.addItem(item)
-
-    def show_context_menu(self, position):
+    def delete_menu(self, position):
         context_menu = QMenu(self)
         delete_action = context_menu.addAction("Удалить задачу")
         action = context_menu.exec(self.mapToGlobal(position))
@@ -52,15 +44,10 @@ class TaskTracker(QListWidget):
             )
             if reply == QMessageBox.StandardButton.Yes:
                 for task in selected_tasks:
-                    self.remove_task_from_db(task)
+                    task_id = task.data(Qt.ItemDataRole.UserRole)
+                    self.db_manager.delete_task(task_id)
                     self.takeItem(self.row(task))
         else:
             QMessageBox.warning(
                 self, "Ошибка", "Пожалуйста, выберите задачу для удаления."
             )
-
-    def remove_task_from_db(self, item):
-        task_id = item.data(Qt.ItemDataRole.UserRole)
-        cursor = self.db_connection.cursor()
-        cursor.execute("DELETE FROM tasks WHERE id = ?", (task_id,))
-        self.db_connection.commit()
